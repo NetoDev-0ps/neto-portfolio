8d6e8e66a843637eb020e5d43deb30c836b7c9be
Neto Monteiro <netosoft@gmail.com>
2026-02-09 15:04:27 -0300

    Configurando estrutura de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  base: './', // Isso garante que o site funcione em qualquer subpasta ou raiz
 })
