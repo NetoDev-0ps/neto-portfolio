9106f4550758cda40fe484caee25a9b6c6d2909c
Neto Monteiro <netosoft@gmail.com>
2026-02-09 16:26:39 -0300

    Revert "Configurando estrutura de deploy"
    This reverts commit 8d6e8e66a843637eb020e5d43deb30c836b7c9be.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './', // Isso garante que o site funcione em qualquer subpasta ou raiz
+  plugins: [react()],
 })
